cfbeb14b5a98ee6167b79b8c336b4cdfac3917bf
Юрий Данилов <yuriy.ni.danilov@rt.ru>
2023-05-21 22:27:39 +0300

    5-languages

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hello world!')
+const languages = {
+  en: 'en',
+  ru: 'ru',
+  de: 'de',
+};
+
+const defaultReply = 'Неизвестный язык. Перезагрузите страницу чтобы попробовать снова.';
+
+const replies = {
+  en: 'Good afternoon!',
+  ru: 'Добрый день!',
+  de: 'Gutten tag!',
+};
+
+const lang = prompt('Выберете язык', languages.ru);
+console.log(lang);
+
+switch (lang) {
+  case null:
+    break
+  case languages.ru:
+    alert(replies.ru);
+    break;
+  case languages.en:
+    alert(replies.en);
+    break;
+  case languages.de:
+    alert(replies.de);
+    break;
+  default:
+    alert(defaultReply);
+    break;
+}
